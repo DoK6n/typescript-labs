fdd281e9049cdb310d8a4786ca91627be7da9a25
Dok6n <ehrbs2018@gmail.com>
2024-09-03 01:33:22 +0900

    [`libraries/effect/typescript`]: effect로 타입 안전성을 높이기

--- libraries/effect/typescript/src/railway-oriented-programming/index.ts
+++ libraries/effect/typescript/src/railway-oriented-programming/index.ts
@@ -15,12 +15,41 @@ class InvalidError implements IError {
   constructor(public message: string) {}
 }
 
+type User = {
+  id: number
+  name: string
+}
+
 // Railway Oriented Programming 예제 함수
 class DataService {
+  private data: User[] = [
+    { id: 1, name: 'John' },
+    { id: 2, name: 'Jane' },
+    { id: 3, name: 'Doe' },
+  ]
+
   getDataById(id: number) {
-    return id > 0
-      ? Effect.succeed(`Data for id: ${id}`)
-      : Effect.fail(new NotFoundError('ID must be greater than 0'))
+    const data = this.data.find(d => d.id === id)
+
+    return data ? Effect.succeed(data) : Effect.fail(new NotFoundError('ID must be greater than 0'))
+  }
+
+  getDataByName(name: string) {
+    const data = this.data.find(d => d.name === name)
+
+    return Effect.gen(function* (_) {
+      if (name.length < 3) {
+        return yield* _(Effect.fail(new InvalidError('Name must be at least 3 characters long')))
+      }
+
+      if (name.length > 10) {
+        return { id: 1, name: 'John' } as User
+      }
+
+      return data
+        ? `Data for name: ${name}`
+        : yield* _(Effect.fail(new NotFoundError('Name not found')))
+    })
   }
 }
 
@@ -29,15 +58,19 @@ class DataController {
 
   getDataById(id: number) {
     const program = this.dataService.getDataById(id)
+    return program
+  }
 
+  getDataByName(name: string) {
+    const program = this.dataService.getDataByName(name)
     return program
   }
 }
 
 const dataController = new DataController(new DataService())
 
-const result = dataController.getDataById(0)
+const resultById = dataController.getDataById(0)
+const responseById = Effect.runPromiseExit(resultById) // Promise<Exit<string, NotFoundError>>
 
-Effect.runPromise(result)
-  .then(data => console.log(data))
-  .catch(error => console.error(error))
+const resultByName = dataController.getDataByName('John')
+const responseByName = Effect.runPromiseExit(resultByName) // Promise<Exit<string | User, NotFoundError | InvalidError>>
