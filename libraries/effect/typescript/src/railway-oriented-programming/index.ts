8243171b56bfa8df4b172ab2dab73eaea44ef2dd
Dok6n <ehrbs2018@gmail.com>
2024-09-05 00:10:27 +0900

    [`libraries/effect/typescript/rop`]: Effect runPromiseExit 결과 로그 표시

--- libraries/effect/typescript/src/railway-oriented-programming/index.ts
+++ libraries/effect/typescript/src/railway-oriented-programming/index.ts
@@ -28,27 +28,29 @@ class DataService {
     { id: 3, name: 'Doe' },
   ]
 
-  getDataById(id: number) {
+  async getDataById(id: number) {
     const data = this.data.find(d => d.id === id)
 
-    return data ? Effect.succeed(data) : Effect.fail(new NotFoundError('ID must be greater than 0'))
+    return data ? Effect.succeed(data) : Effect.fail(new NotFoundError('ID는 0보다 커야 합니다'))
   }
 
-  getDataByName(name: string) {
+  async getDataByName(name: string) {
     const data = this.data.find(d => d.name === name)
 
     return Effect.gen(function* (_) {
-      if (name.length < 3) {
-        return yield* _(Effect.fail(new InvalidError('Name must be at least 3 characters long')))
+      if (name.length < 2) {
+        return yield* _(Effect.fail(new InvalidError('이름은 최소 2글자 이상이어야 합니다')))
       }
 
-      if (name.length > 10) {
-        return { id: 1, name: 'John' } as User
+      if (name.length > 20) {
+        return yield* _(Effect.fail(new InvalidError('이름은 20글자를 초과할 수 없습니다')))
       }
 
-      return data
-        ? `Data for name: ${name}`
-        : yield* _(Effect.fail(new NotFoundError('Name not found')))
+      if (!data) {
+        return yield* _(Effect.fail(new NotFoundError('해당 이름의 사용자를 찾을 수 없습니다')))
+      }
+
+      return yield* _(Effect.succeed(data))
     })
   }
 }
@@ -56,21 +58,87 @@ class DataService {
 class DataController {
   constructor(private dataService: DataService) {}
 
-  getDataById(id: number) {
-    const program = this.dataService.getDataById(id)
+  async getDataById(id: number) {
+    const program = Effect.runPromiseExit(await this.dataService.getDataById(id))
     return program
   }
 
-  getDataByName(name: string) {
-    const program = this.dataService.getDataByName(name)
+  async getDataByName(name: string) {
+    const program = Effect.runPromiseExit(await this.dataService.getDataByName(name))
     return program
   }
 }
 
-const dataController = new DataController(new DataService())
+const main = async () => {
+  const dataController = new DataController(new DataService())
 
-const resultById = dataController.getDataById(0)
-const responseById = Effect.runPromiseExit(resultById) // Promise<Exit<string, NotFoundError>>
+  const resultById0 = await dataController.getDataById(0) // Exit<User, NotFoundError>
+  const resultById2 = await dataController.getDataById(2) // Exit<User, NotFoundError>
+  const resultByName = await dataController.getDataByName('John') // Exit<User, InvalidError | NotFoundError>
+  const resultByNameOver20 = await dataController.getDataByName('aaaaaaaaaaaaaaaaaaaa') // Exit<User, InvalidError | NotFoundError>
+  const resultByNameUnder2 = await dataController.getDataByName('a') // Exit<User, InvalidError | NotFoundError>
 
-const resultByName = dataController.getDataByName('John')
-const responseByName = Effect.runPromiseExit(resultByName) // Promise<Exit<string | User, NotFoundError | InvalidError>>
+  console.log('resultById0', resultById0 + '\n')
+  // resultById0 {
+  //   "_id": "Exit",
+  //   "_tag": "Failure",
+  //   "cause": {
+  //     "_id": "Cause",
+  //     "_tag": "Fail",
+  //     "failure": {
+  //       "message": "ID는 0보다 커야 합니다",
+  //       "_tag": "NotFoundError"
+  //     }
+  //   }
+  // }
+
+  console.log('resultById2', resultById2 + '\n')
+  // resultById2 {
+  //   "_id": "Exit",
+  //   "_tag": "Success",
+  //   "value": {
+  //     "id": 2,
+  //     "name": "Jane"
+  //   }
+  // }
+
+  console.log('resultByName', resultByName + '\n')
+  // resultByName {
+  //   "_id": "Exit",
+  //   "_tag": "Success",
+  //   "value": {
+  //     "id": 1,
+  //     "name": "John"
+  //   }
+  // }
+
+  console.log('resultByNameOver20', resultByNameOver20 + '\n')
+  // resultByNameOver20 {
+  //   "_id": "Exit",
+  //   "_tag": "Failure",
+  //   "cause": {
+  //     "_id": "Cause",
+  //     "_tag": "Fail",
+  //     "failure": {
+  //       "message": "해당 이름의 사용자를 찾을 수 없습니다",
+  //       "_tag": "NotFoundError"
+  //     }
+  //   }
+  // }
+
+  console.log('resultByNameUnder2', resultByNameUnder2)
+  // resultByNameUnder2 {
+  //   _id: 'Exit',
+  //   _tag: 'Failure',
+  //   cause: {
+  //     _id: 'Cause',
+  //     _tag: 'Fail',
+  //     failure: InvalidError {
+  //       message: '이름은 최소 2글자 이상이어야 합니다',
+  //       _tag: 'InvalidError'
+  //     }
+  //   }
+  // }
+}
+
+main()
